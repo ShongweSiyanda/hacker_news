9f20c669eb2f179f15a68c996af879e32fc6fd33
shongwesiyanda <shongwesiyanda@gmail.com>
2022-10-23 12:11:27 +0200

    Stories DB Migration Table

---
+++ database/migrations/2022_10_23_085002_create_stories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->bigInteger('story_id')->unique()->primary();
+            $table->text('type');
+            $table->text('by');
+            $table->dateTime('time');
+            $table->integer('score');
+            $table->text('title');
+            $table->text('category')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stories');
+    }
+};
